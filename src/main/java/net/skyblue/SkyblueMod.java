3784187dda11eae9a6d13319574ac4af7bd20577
Floi1 <hamnsar@gmail.com>
2025-10-17 17:35:39 +0100

    info

--- src/main/java/net/skyblue/SkyblueMod.java
+++ src/main/java/net/skyblue/SkyblueMod.java
@@ -45,7 +45,7 @@ import java.util.function.Supplier;
 @Mod(modid = SkyblueMod.MODID, version = SkyblueMod.VERSION)
 public class SkyblueMod {
 	public static final String MODID = "skyblue";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "0.0.1";
 	public static final SimpleNetworkWrapper PACKET_HANDLER = NetworkRegistry.INSTANCE.newSimpleChannel("skyblue:a");
 	@SidedProxy(clientSide = "net.skyblue.ClientProxySkyblueMod", serverSide = "net.skyblue.ServerProxySkyblueMod")
 	public static IProxySkyblueMod proxy;
