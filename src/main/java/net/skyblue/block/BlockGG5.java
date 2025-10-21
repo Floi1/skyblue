bd711803ff14a2d8249269f71d913059fa149a08
Floi1 <hamnsar@gmail.com>
2025-10-21 19:03:39 +0100

    Dim G

--- src/main/java/net/skyblue/block/BlockGG5.java
+++ src/main/java/net/skyblue/block/BlockGG5.java
@@ -1,7 +1,6 @@
 
 package net.skyblue.block;
 
-import net.skyblue.procedure.ProcedureGreen4EntityWalksOnTheBlock;
 import net.skyblue.creativetab.TabSkyblue;
 import net.skyblue.ElementsSkyblueMod;
 
@@ -11,42 +10,36 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 
-import net.minecraft.world.World;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.Item;
-import net.minecraft.entity.Entity;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.Block;
 
-import java.util.Map;
-import java.util.HashMap;
-
 @ElementsSkyblueMod.ModElement.Tag
-public class BlockGreen4 extends ElementsSkyblueMod.ModElement {
-	@GameRegistry.ObjectHolder("skyblue:green_4")
+public class BlockGG5 extends ElementsSkyblueMod.ModElement {
+	@GameRegistry.ObjectHolder("skyblue:gg_5")
 	public static final Block block = null;
-	public BlockGreen4(ElementsSkyblueMod instance) {
-		super(instance, 192);
+	public BlockGG5(ElementsSkyblueMod instance) {
+		super(instance, 211);
 	}
 
 	@Override
 	public void initElements() {
-		elements.blocks.add(() -> new BlockCustom().setRegistryName("green_4"));
+		elements.blocks.add(() -> new BlockCustom().setRegistryName("gg_5"));
 		elements.items.add(() -> new ItemBlock(block).setRegistryName(block.getRegistryName()));
 	}
 
 	@SideOnly(Side.CLIENT)
 	@Override
 	public void registerModels(ModelRegistryEvent event) {
-		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(block), 0, new ModelResourceLocation("skyblue:green_4", "inventory"));
+		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(block), 0, new ModelResourceLocation("skyblue:gg_5", "inventory"));
 	}
 	public static class BlockCustom extends Block {
 		public BlockCustom() {
 			super(Material.IRON);
-			setUnlocalizedName("green_4");
+			setUnlocalizedName("gg_5");
 			setSoundType(SoundType.METAL);
 			setHarvestLevel("pickaxe", 1);
 			setHardness(11F);
@@ -54,19 +47,6 @@ public class BlockGreen4 extends ElementsSkyblueMod.ModElement {
 			setLightLevel(0F);
 			setLightOpacity(255);
 			setCreativeTab(TabSkyblue.tab);
-		}
-
-		@Override
-		public void onEntityWalk(World world, BlockPos pos, Entity entity) {
-			super.onEntityWalk(world, pos, entity);
-			int x = pos.getX();
-			int y = pos.getY();
-			int z = pos.getZ();
-			{
-				Map<String, Object> $_dependencies = new HashMap<>();
-				$_dependencies.put("entity", entity);
-				ProcedureGreen4EntityWalksOnTheBlock.executeProcedure($_dependencies);
-			}
 		}
 	}
 }
